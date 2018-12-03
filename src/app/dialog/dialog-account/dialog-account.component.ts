082121a31dcad0466e7c030b948f9f01723e3c51
Cl3r1k <Cl3r1k2@gmail.com>
2018-12-03 09:08:23 +0600

    ✔ Added 'Security' tab and elements Updates in 'DialogAccountComponent' (code, styles, template) Minor changes

--- src/app/dialog/dialog-account/dialog-account.component.ts
+++ src/app/dialog/dialog-account/dialog-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
@@ -9,14 +10,27 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 })
 export class DialogAccountComponent implements OnInit {
 
+    public formPassword: FormGroup;
+
     isFirstTab = true;
     editNameState = false;
     editEmailState = false;
     currentPassword = '';
     newPassword = '';
     newPasswordConfirm = '';
+    formValid = false;
 
-    constructor(public dialogRef: MatDialogRef<DialogAccountComponent>, @Inject(MAT_DIALOG_DATA) public data) { }
+    constructor(
+        public dialogRef: MatDialogRef<DialogAccountComponent>,
+        @Inject(MAT_DIALOG_DATA) public data,
+        private _formBuilder: FormBuilder
+    ) {
+        this.formPassword = _formBuilder.group({
+            password: ['', Validators.required],
+            passwordNew: ['', Validators.required],
+            passwordNewConfirm: ['', Validators.required]
+        });
+    }
 
     ngOnInit() {
         console.log('data: ', this.data);
@@ -33,5 +47,7 @@ export class DialogAccountComponent implements OnInit {
     setEditEmailState(state) {
         this.editEmailState = state;
     }
+
+    // TODO: Use reactive forms for passwords fields
 
 }
