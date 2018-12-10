a3a8b13e5c7f0511ab4343936774be6e5f243603
Cl3r1k <Cl3r1k2@gmail.com>
2018-12-10 09:39:44 +0600

    Updates in 'MatchValidation' Updates in 'DialogAccountComponent' (code, styles, template) Minor changes

--- src/app/dialog/dialog-account/dialog-account.component.ts
+++ src/app/dialog/dialog-account/dialog-account.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 // Utils
-// import { MatchValidation } from '@app/_common/match-validation';
-import { MustMatch } from '@app/_common/match-validation';
+import { MatchValidation } from '@app/_common/match-validation';
 
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
@@ -37,8 +36,7 @@ export class DialogAccountComponent implements OnInit {
             password: ['', Validators.required],
             passwordConfirm: ['', Validators.required]
         }, {
-                // validator: MatchValidation.validate('password', 'passwordConfirm')
-                validator: MustMatch('password', 'passwordConfirm')
+                validator: MatchValidation.validate('password', 'passwordConfirm')
             });
     }
 
@@ -61,55 +59,15 @@ export class DialogAccountComponent implements OnInit {
 
     changePassword() {
 
-        console.log('%cformPassword.errors: ', this.consoleTextColorComponent, this.formPassword.errors);
-
-        console.log('%cfrmPwd.ctls.passdConfirm.err: ', this.consoleTextColorComponent, this.formPassword.controls.passwordConfirm.errors);
+        // console.log('%cformPassword.errors: ', this.consoleTextColorComponent, this.formPassword.errors);
+        // console.log('%cfrmPwd.ctls.pwdCnfrm.err: ', this.consoleTextColorComponent, this.formPassword.controls.passwordConfirm.errors);
 
         if (this.formPassword.invalid) {
             this.showPasswordsInputErrors = true;
             return;
         }
 
-        console.log('%cPassword changes!', this.consoleTextColorComponent);
+        console.log('%cPassword changed!', this.consoleTextColorComponent);
     }
-
-    // TODO: Write custom validation
-    // getErrorMessage(sender: string) {
-    //     let errorMessage = '';
-
-    //     if (sender === 'password') {
-    //         errorMessage = this.formPassword.get('password').hasError('required') ? 'You must enter a value' : '';
-    //         this.passwordInputError = true;
-    //     }
-
-    //     if (sender === 'passwordNew') {
-    //         errorMessage = this.formPassword.get('passwordNew').hasError('required') ? 'You must enter a value' : '';
-    //         this.passwordNewInputError = true;
-    //     }
-
-    //     if (sender === 'passwordNewConfirm') {
-    //         errorMessage = this.formPassword.get('passwordNewConfirm').hasError('required') ? 'You must enter a value' : '';
-    //         this.passwordNewConfirmInputError = true;
-    //     }
-
-    //     if (sender === 'passwordNew' || sender === 'passwordNewConfirm') {
-    //         if (this.formPassword.get('passwordNew').valid && this.formPassword.get('passwordNewConfirm').valid) {
-    //             if (this.formPassword.get('passwordNew').value !== this.formPassword.get('passwordNewConfirm').value) {
-    //                 errorMessage = 'Password and confirm not equal';
-    //                 this.passwordNewInputError = true;
-    //                 this.passwordNewConfirmInputError = true;
-    //             }
-    //         }
-    //     }
-
-    //     if (!this.passwordInputError && !this.passwordNewInputError && !this.passwordNewConfirmInputError) {
-    //         console.log('%cformValid is TRUE', this.consoleTextColorComponent);
-    //         this.formValid = true;
-    //     } else {
-    //         this.formValid = false;
-    //     }
-
-    //     return errorMessage;
-    // }
 
 }
