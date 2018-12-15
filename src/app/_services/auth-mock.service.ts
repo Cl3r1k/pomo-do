95429b6ed27cf0588276f8e044fa0968e797fce0
Cl3r1k <Cl3r1k2@gmail.com>
2018-12-15 09:14:14 +0600

    Added 'AuthMockService' Update in 'TodoTitleComponent' tests Minor changes

---
+++ src/app/_services/auth-mock.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthMockService {
+
+    constructor() { }
+
+    public doSignOut(): null {
+        return null;
+    }
+
+}
--- src/app/todo-title/todo-title.component.spec.ts
+++ src/app/todo-title/todo-title.component.spec.ts
@@ -1,7 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+// Services
+import { AuthService } from '@app/_services/auth.service';
+import { AuthMockService } from '@app/_services/auth-mock.service';
+
+// Directives
+import { TooltipDirective } from '@app/_directives/tooltip.directive';
 
 // Components
 import { TodoTitleComponent } from '@app/todo-title/todo-title.component';
+
+// Modules
+import { MatDialogModule } from '@angular/material';
 
 describe('Component: TodoTitleComponent', () => {
     let component: TodoTitleComponent;
@@ -9,7 +20,13 @@ describe('Component: TodoTitleComponent', () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [TodoTitleComponent]
+            imports: [RouterTestingModule, MatDialogModule],
+            declarations: [TodoTitleComponent, TooltipDirective],
+            providers: [
+                {
+                    provide: AuthService,
+                    useClass: AuthMockService
+                }]
         })
             .compileComponents();
     }));
@@ -36,6 +53,47 @@ describe('Component: TodoTitleComponent', () => {
         // Act
 
         // Assert
-        expect(compiled.querySelector('h1').textContent).toContain('Todo');
+        expect(compiled.querySelector('h1').textContent).toContain('Pomodo');
     }));
+
+    describe(`#changeSyncState()`, () => {
+        it(`should be...`, async(() => {
+            // TODO: Update test when this method will be finished
+            // Arrange
+
+            // Act
+
+            // Assert
+        }));
+    });
+
+    describe(`#toggleSubmenuState()`, () => {
+        it(`should reverse 'showSubmenuState' and emit 'subMenuStateAppTitleEmitter' event`, async(() => {
+            // Arrange
+            let subMenuState = false;
+
+            // Act
+            component.showSubmenuState = false;
+            component.subMenuStateAppTitleEmitter.subscribe((value: boolean) => subMenuState = value);    // Subscribe to 'subMenuState' evt
+            component.toggleSubmenuState();
+
+            // Assert
+            expect(component.showSubmenuState).toEqual(true);
+            expect(subMenuState).toEqual(true);
+        }));
+    });
+
+    describe(`#doSignOut()`, () => {
+        it(`should call method 'purgeAuth()' and 'sessionStorageService.destroy()'`, () => {
+            // Arrange
+            // authService = { isSignedIn: () => true };
+
+            // Act
+            spyOn(component, 'doSignOut');
+            component.doSignOut();
+
+            // Assert
+            // expect(service.doSignOut).toHaveBeenCalled();
+        });
+    });
 });
