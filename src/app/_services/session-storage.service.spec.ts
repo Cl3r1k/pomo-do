532d537b75c6b95eed18854584cd039cefdc5a57
Cl3r1k <Cl3r1k2@gmail.com>
2018-12-18 09:12:39 +0600

    ✔ Updated tests for 'SessionStorageService' Minor changes

--- src/app/_services/session-storage.service.spec.ts
+++ src/app/_services/session-storage.service.spec.ts
@@ -23,18 +23,50 @@ describe('Service: SessionStorageService', () => {
         expect(sessionStorageService).toBeTruthy();
     });
 
-    describe(`#destroy()`, () => {
-        it(`should clear 'accessToken' and 'name'`, () => {
+    describe(`#init()`, () => {
+        it(`should set 'session_object' to initial values`, () => {
             // Arrange
-            sessionStorageService.accessToken = 'Access.Token';
-            sessionStorageService.name = 'Access.Name';
+
+            // Act
+            sessionStorageService.init();
+
+            // Assert
+            expect(sessionStorageService.session_object.expire_time).toEqual('');
+            expect(sessionStorageService.session_object.token).toEqual('');
+            expect(sessionStorageService.session_object.account.avatar).toEqual('some_kind_of_avatar');
+            expect(sessionStorageService.session_object.account.display_name).toEqual('');
+            expect(sessionStorageService.session_object.account.email).toEqual('some_kind_of@email.com');
+            expect(sessionStorageService.session_object.account.email_verified).toEqual(false);
+            expect(sessionStorageService.session_object.account.id).toEqual('1');
+            expect(sessionStorageService.session_object.account.jwToken).toEqual('');
+            expect(sessionStorageService.session_object.account.name).toEqual('');
+            expect(sessionStorageService.session_object.account.username).toEqual('');
+        });
+    });
+
+    describe(`#destroy()`, () => {
+        it(`should clear 'session_object'`, () => {
+            // Arrange
+            sessionStorageService.session_object.account.jwToken = 'Access.Token';
+            sessionStorageService.session_object.account.name = 'Access.Name';
 
             // Act
             sessionStorageService.destroy();
 
             // Assert
-            expect(sessionStorageService.accessToken).toEqual(null);
-            expect(sessionStorageService.name).toEqual(null);
+            expect(sessionStorageService.session_object.created_time).toEqual(null);
+            expect(sessionStorageService.session_object.expire_time).toEqual(null);
+            expect(sessionStorageService.session_object.last_used_time).toEqual(null);
+            expect(sessionStorageService.session_object.token).toEqual(null);
+            expect(sessionStorageService.session_object.account.avatar).toEqual(null);
+            expect(sessionStorageService.session_object.account.display_name).toEqual(null);
+            expect(sessionStorageService.session_object.account.email).toEqual(null);
+            expect(sessionStorageService.session_object.account.email_verified).toEqual(false);
+            expect(sessionStorageService.session_object.account.id).toEqual(null);
+            expect(sessionStorageService.session_object.account.jwToken).toEqual(null);
+            expect(sessionStorageService.session_object.account.name).toEqual(null);
+            expect(sessionStorageService.session_object.account.username).toEqual(null);
+            expect(sessionStorageService.session_object.account.register_time).toEqual(null);
         });
     });
 
