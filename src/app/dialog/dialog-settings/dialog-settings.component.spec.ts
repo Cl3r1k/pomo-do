5dec0d9e3f5af677891425e3c3c0a6474ee23861
Cl3r1k <Cl3r1k2@gmail.com>
2018-12-29 09:22:16 +0600

    ✔ Updated tests for 'DialogSettingsComponent' ✔ Completed dialog for 'settings-item' Minor changes

--- src/app/dialog/dialog-settings/dialog-settings.component.spec.ts
+++ src/app/dialog/dialog-settings/dialog-settings.component.spec.ts
@@ -84,4 +84,161 @@ describe('Component: DialogSettingsComponent', () => {
         expect(component.monthlyGoalSaveText).toEqual('Saving');
         expect(component.proStatus).toEqual(false);
     });
+
+    it(`form invalid when empty (passwords fields are empty)`, () => {
+        // Arrange
+
+        // Act
+        component.formGoal.reset();
+
+        // Assert
+        expect(component.formGoal.valid).toBeFalsy();
+    });
+
+    describe(`#'dailyGoal' field tests: `, () => {
+        it(`'dailyGoal' field validity`, () => {
+            // Arrange
+
+            // Act
+            component.formGoal.reset();
+            const dailyGoal = component.formGoal.controls['dailyGoal'];
+
+            // Assert
+            expect(dailyGoal.valid).toBeFalsy();
+        });
+
+        it(`'dailyGoal' field 'required' validity`, () => {
+            // Arrange
+            let errors = {};
+
+            // Act
+            component.formGoal.reset();
+            const dailyGoal = component.formGoal.controls['dailyGoal'];
+            errors = dailyGoal.errors || {};
+
+            // Assert
+            expect(errors['required']).toBeTruthy();
+        });
+
+        it(`'dailyGoal' field with data should be valid and 'required' should be undefined`, () => {
+            // Arrange
+            let errors = {};
+
+            // Act
+            const dailyGoal = component.formGoal.controls['dailyGoal'];
+            dailyGoal.setValue(8);
+            errors = dailyGoal.errors || {};
+
+            // Assert
+            expect(dailyGoal.valid).toBeTruthy();
+            expect(errors['required']).toEqual(undefined);
+        });
+    });
+
+    describe(`#'weeklyGoal' field tests: `, () => {
+        it(`'weeklyGoal' field validity`, () => {
+            // Arrange
+
+            // Act
+            component.formGoal.reset();
+            const weeklyGoal = component.formGoal.controls['weeklyGoal'];
+
+            // Assert
+            expect(weeklyGoal.valid).toBeFalsy();
+        });
+
+        it(`'weeklyGoal' field 'required' validity`, () => {
+            // Arrange
+            let errors = {};
+
+            // Act
+            component.formGoal.reset();
+            component.formGoal.controls['weeklyGoal'].enable();
+            const weeklyGoal = component.formGoal.controls['weeklyGoal'];
+            errors = weeklyGoal.errors || {};
+
+            // Assert
+            expect(errors['required']).toBeTruthy();
+        });
+
+        it(`'weeklyGoal' field with data should be valid and 'required' should be undefined`, () => {
+            // Arrange
+            let errors = {};
+
+            // Act
+            component.formGoal.controls['weeklyGoal'].enable();
+            const weeklyGoal = component.formGoal.controls['weeklyGoal'];
+            weeklyGoal.setValue(40);
+            errors = weeklyGoal.errors || {};
+
+            // Assert
+            expect(weeklyGoal.valid).toBeTruthy();
+            expect(errors['required']).toEqual(undefined);
+        });
+    });
+
+    describe(`#'monthlyGoal' field tests: `, () => {
+        it(`'monthlyGoal' field validity`, () => {
+            // Arrange
+
+            // Act
+            component.formGoal.reset();
+            const monthlyGoal = component.formGoal.controls['monthlyGoal'];
+
+            // Assert
+            expect(monthlyGoal.valid).toBeFalsy();
+        });
+
+        it(`'monthlyGoal' field 'required' validity`, () => {
+            // Arrange
+            let errors = {};
+
+            // Act
+            component.formGoal.reset();
+            component.formGoal.controls['monthlyGoal'].enable();
+            const monthlyGoal = component.formGoal.controls['monthlyGoal'];
+            errors = monthlyGoal.errors || {};
+
+            // Assert
+            expect(errors['required']).toBeTruthy();
+        });
+
+        it(`'monthlyGoal' field with data should be valid and 'required' should be undefined`, () => {
+            // Arrange
+            let errors = {};
+
+            // Act
+            component.formGoal.controls['monthlyGoal'].enable();
+            const monthlyGoal = component.formGoal.controls['monthlyGoal'];
+            monthlyGoal.setValue(160);
+            errors = monthlyGoal.errors || {};
+
+            // Assert
+            expect(monthlyGoal.valid).toBeTruthy();
+            expect(errors['required']).toEqual(undefined);
+        });
+    });
+
+    describe(`#view tests`, () => {
+        // TODO: Improve this test for closing dialog
+        // look here https://github.com/angular/material2/blob/master/src/lib/dialog/dialog.spec.ts#L178
+        // here https://github.com/angular/material2/blob/d1128febe6d23f1a1f20446692bc2a8358e8b8cf/src/lib/dialog/dialog.spec.ts#L116-L116
+        // and here https://medium.com/@aleixsuau/testing-angular-components-with-material-dialog-mddialog-1ae658b4e4b3
+        // it(`clicking on button.dialog__close-btn should call method 'dialogRef.close()' (async)`, async () => {
+        //     // Arrange
+
+        //     // Act
+        //     spyOn(component.dialogRef, 'close');
+        //     if (dialogCloseBtnEl instanceof HTMLElement) {
+        //         dialogCloseBtnEl.click();
+        //     } else {
+        //         dialogCloseBtnEl.triggerEventHandler('click', { button: 0 });
+        //     }
+
+        //     // Assert
+        //     fixture.whenStable().then(() => {
+        //         expect(component.dialogRef.close).toHaveBeenCalled();
+        //     });
+        // });
+    });
 });
