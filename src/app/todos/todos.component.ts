94c71cb0b7131788aee253be27e4fb79446cb958
Cl3r1k <Cl3r1k2@gmail.com>
2019-01-18 07:59:16 +0600

    Updates in 'MainComponent' (template) Updates in 'TodosComponent' (code, template) Minor changes

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
+// Models
+import { ToDo } from '@app/_models/to-do';
 
 @Component({
     selector: 'app-todos',
@@ -8,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+    consoleTextColorComponent = 'color: cadetblue;';
+
+    @Input() todosAllAmount: number;
+    @Input() todosAllCompleted: boolean;
+    @Input() hashTagToFilter: string;
+
+    @Output() addTodosComponentEmitter: EventEmitter<ToDo> = new EventEmitter();
+    @Output() toggleAllTodosComponentEmitter: EventEmitter<boolean> = new EventEmitter();
+    @Output() toggleAllHoverStateTodosComponentEmitter: EventEmitter<boolean> = new EventEmitter();
+
+    @Input() todos: ToDo[];
+    @Input() todosToView = [[]];
+
+    @Output() toggleCompleteTodosComponentEmitter: EventEmitter<ToDo> = new EventEmitter();
+    @Output() updateTodosComponentEmitter: EventEmitter<ToDo> = new EventEmitter();
+    @Output() moreTodosComponentEmitter: EventEmitter<ToDo> = new EventEmitter();
+    @Output() pinTodosComponentEmitter: EventEmitter<ToDo> = new EventEmitter();
+    @Output() removeTodosComponentEmitter: EventEmitter<ToDo> = new EventEmitter();
+    @Output() moveTodosComponentEmitter: EventEmitter<ToDo[]> = new EventEmitter();
+    @Output() clearTodosComponentEmitter: EventEmitter<boolean> = new EventEmitter();
+    @Output() clearHoverStateTodosComponentEmitter: EventEmitter<boolean> = new EventEmitter();
+
     constructor() { }
 
     ngOnInit() {
+    }
+
+    onAddTodo(todo: ToDo) {
+        this.addTodosComponentEmitter.emit(todo);    // Emit the addTodo event to 'MainComponent'
+    }
+
+    toggleAllTodos(toggleState: boolean) {
+        this.toggleAllTodosComponentEmitter.emit(toggleState);    // Emit the toggleAll event to 'MainComponent'
+    }
+
+    setToggleAllHoverState(toggleAllHoverState: boolean) {
+        this.toggleAllHoverStateTodosComponentEmitter.emit(toggleAllHoverState);    // Emit the toggleAllHoverState event to 'MainComponent'
     }
 
 }
