0d45c4c64db14816c65552c0d1b5729bb212a516
Cl3r1k <Cl3r1k2@gmail.com>
2019-01-29 09:13:14 +0600

    Added file '_pomo_header_app.scss' Moved styles for 'PomoHeaderComponent' to '_pomo_header_app.scss' Updates in 'PomoHeaderComponent' (template, code, styles) Minor changes

--- src/app/pomos/pomo-header/pomo-header.component.ts
+++ src/app/pomos/pomo-header/pomo-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'app-pomo-header',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PomoHeaderComponent implements OnInit {
 
+    @Output() startPomoPomoHeaderComponentEmitter: EventEmitter<boolean> = new EventEmitter();
+
+    pomoStarted = false;
+
     constructor() { }
 
     ngOnInit() {
+    }
+
+    startPomo() {
+        this.startPomoPomoHeaderComponentEmitter.emit(true);    // Emit the 'startPomo' event to 'PomosComponent'
     }
 
 }
