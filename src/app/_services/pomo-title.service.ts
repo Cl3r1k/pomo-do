11f208086960a3a1774ab2d1d0694770f5468a12
Cl3r1k <Cl3r1k2@gmail.com>
2019-03-16 09:06:00 +0600

    ✔ The selected 'todo' should be changed visually ✔ Changes 'border-top' for selected 'todo' Updates in 'PomoTitleService' Minor changes

--- src/app/_services/pomo-title.service.ts
+++ src/app/_services/pomo-title.service.ts
@@ -10,6 +10,8 @@ export class PomoTitleService {
 
     consoleTextColorService = 'color: salmon;';
 
+    listOfUsedTodos: Object[] = [];
+
     currentPomoState = 0;
     pomoTitle = 'Some kind of title';
 
@@ -21,5 +23,31 @@ export class PomoTitleService {
 
     updatePomoTitleWithTodo(todo: ToDo) {
         console.log('%cPomoTitleService - todo: ', this.consoleTextColorService, todo);
+
+        let todoTitleState = 0;
+        let todoTitleIndex = 0;
+        this.listOfUsedTodos.map(item => {
+            if (item['innerId'] === todo.inner_id) {
+                todoTitleState = item['todoTitleState'];
+            }
+
+            if (!todoTitleState) {
+                todoTitleIndex++;
+            }
+        });
+
+        if (todoTitleState === 0) {
+            this.listOfUsedTodos.push({ innerId: todo.inner_id, todoTitleState: 1 });
+            this.processTodoTitle(todo.title, true);
+        }
+
+        if (todoTitleState === 1) {
+            this.listOfUsedTodos.splice(todoTitleIndex, 1);
+            this.processTodoTitle(todo.title, false);
+        }
+    }
+
+    processTodoTitle(todoTitle: string, state: boolean) {
+        console.log('%cPomoTitleService - listOfUsedTodos: ', this.consoleTextColorService, this.listOfUsedTodos);
     }
 }
