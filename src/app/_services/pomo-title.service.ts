c27edd07ec99f2c8d380cd43f34855788527b3c1
Cl3r1k <Cl3r1k2@gmail.com>
2019-03-18 10:12:20 +0600

    Updates in 'PomoTitleService' Minor changes

--- src/app/_services/pomo-title.service.ts
+++ src/app/_services/pomo-title.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 // Models
 import { ToDo } from '@app/_models/to-do';
 
+// Services
+import { TodoOrderService } from '@app/_services/todo-order.service';
+
 @Injectable({
     providedIn: 'root'
 })
@@ -14,8 +17,9 @@ export class PomoTitleService {
 
     currentPomoState = 0;
     pomoTitle = 'Some kind of title';
+    pomoTitleTodosPart = '';
 
-    constructor() { }
+    constructor(private _todoOrderService: TodoOrderService) { }
 
     setPomoState(state: number) {
         this.currentPomoState = state;
@@ -23,6 +27,16 @@ export class PomoTitleService {
 
     updatePomoTitleWithTodo(todo: ToDo) {
         console.log('%cPomoTitleService - todo: ', this.consoleTextColorService, todo);
+
+        this.pomoTitleTodosPart = this.parseTodosTitle(todo);
+
+        if (this.pomoTitleTodosPart) {
+            // this.pomoTitle = this.pomoTitleTodosPart + this.pomoTitle;
+        }
+    }
+
+    parseTodosTitle(todo: ToDo) {
+        let tmpPomoTitleWithTodos = '';
 
         let todoTitleState = 0;
         let todoTitleIndex = 0;
@@ -38,15 +52,28 @@ export class PomoTitleService {
 
         if (todoTitleState === 0) {
             this.listOfUsedTodos.push({ innerId: todo.inner_id, todoTitleState: 1 });
-            this.processTodoTitle(todo.title, true);
+            const orderList = this._todoOrderService.getOrder();
+
+            console.log('%cPomoTitleService - inner_id: ', this.consoleTextColorService, todo.inner_id);
+            orderList.filter(item => {
+
+                const isPresentInnerId = item === todo.inner_id;
+
+                console.log('%cPomoTitleService - isPresentInnerId: ', this.consoleTextColorService, isPresentInnerId);
+                return item !== todo.inner_id;
+            });
+
+            console.log('%cPomoTitleService - orderList: ', this.consoleTextColorService, orderList);
         }
 
         if (todoTitleState === 1) {
             this.listOfUsedTodos.splice(todoTitleIndex, 1);
-            this.processTodoTitle(todo.title, false);
+            // this.processTodoTitle(todo.title, false);
         }
 
         // if todoTitleState = 2 -> do nothing in the service
+
+        return tmpPomoTitleWithTodos;
     }
 
     processTodoTitle(todoTitle: string, state: boolean) {
@@ -61,6 +88,7 @@ export class PomoTitleService {
             // Test variant when single todo selected
             // Test variant when multiple todos selected, and unselected in defferent sequence
             // Test -> todo1 sel -> todo2 sel -> unsel todo2 - case (in the end shouldn't be +)
+            // Selected todo should be added to title according to his position in the 'todo-list'
             const substrIndex = this.pomoTitle.indexOf(todoTitle);
             console.log('%cPomoTitleService - substrIndex: ', this.consoleTextColorService, substrIndex);
             if (substrIndex !== -1) {
