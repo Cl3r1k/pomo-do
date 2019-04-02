836ca85546b57014781d9863fa0ee70711be427c
Cl3r1k <Cl3r1k2@gmail.com>
2019-04-02 09:20:25 +0600

    Updates in tests for 'PomoTitleService' Minor changes

--- src/app/_services/pomo-title.service.spec.ts
+++ src/app/_services/pomo-title.service.spec.ts
@@ -1,12 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
+// Services
 import { PomoTitleService } from './pomo-title.service';
+import { TodoOrderService } from '@app/_services/todo-order.service';
+import { TodoOrderMockService } from '@app/_services/todo-order-mock.service';
 
-describe('PomoTitleService', () => {
-    beforeEach(() => TestBed.configureTestingModule({}));
+describe('Service: PomoTitleService', () => {
+
+    let service: PomoTitleService;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            providers: [
+                {
+                    provide: TodoOrderService,
+                    useClass: TodoOrderMockService
+                }
+            ]
+        });
+
+        service = TestBed.get(PomoTitleService);
+    });
 
     it('should be created', () => {
-        const service: PomoTitleService = TestBed.get(PomoTitleService);
+        // Arrange
+
+        // Act
+
+        // Assert
         expect(service).toBeTruthy();
     });
 });
