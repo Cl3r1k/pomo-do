221fd5561a69015dc4d0663f61af7ba6d7775686
Cl3r1k <Cl3r1k2@gmail.com>
2019-04-13 22:42:22 +0600

    Updates in tests for 'PomoHeaderComponent' Minor changes

--- src/app/pomos/pomo-header/pomo-header.component.spec.ts
+++ src/app/pomos/pomo-header/pomo-header.component.spec.ts
@@ -1,12 +1,16 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
+// Models
+import { ToDo } from '@app/_models/to-do';
+
 // Components
 import { PomoHeaderComponent } from './pomo-header.component';
 
 // Services
 import { TodoOrderService } from '@app/_services/todo-order.service';
 import { TodoOrderMockService } from '@app/_services/todo-order-mock.service';
+import { PomoStateService } from '@app/_services/pomo-state.service';
 
 // Modules
 import { MatDialogModule, MatDialog } from '@angular/material';
@@ -18,6 +22,8 @@ describe('Component: PomoHeaderComponent', () => {
     let component: PomoHeaderComponent;
     let fixture: ComponentFixture<PomoHeaderComponent>;
     let dialog: MatDialogMock;
+    let pomoStateService: PomoStateService;
+    let expectedTodo: ToDo;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -41,6 +47,11 @@ describe('Component: PomoHeaderComponent', () => {
         fixture = TestBed.createComponent(PomoHeaderComponent);
         component = fixture.componentInstance;
 
+        pomoStateService = TestBed.get(PomoStateService);
+        expectedTodo = new ToDo({ id: 1, title: 'Test title in TodoListItemViewComponent', complete: false });
+        expectedTodo.inner_id = '123456789';
+        component.currentTodoPomoHeader = expectedTodo;                    // Lets count that we have todo with 'complete' = false
+
         dialog = TestBed.get(MatDialog);
 
         fixture.detectChanges();
@@ -61,9 +72,105 @@ describe('Component: PomoHeaderComponent', () => {
         // Act
 
         // Assert
-        expect(component.pomoLengthSeconds).toEqual(10);
+        expect(component.pomoLengthSeconds).toEqual(60);
+        expect(component.restLengthSeconds).toEqual(60);
+        // expect(component.counter).toEqual(component.pomoLengthSeconds);
+        expect(component.counterView).toEqual('');
+        expect(component.currentState).toEqual('pomo');
+        expect(component.progressBarPercent).toEqual(0);
+        expect(component.updatedTextHeight).toEqual(false);
+        expect(component.savePomoFocusState).toEqual(false);
     });
 
+    it(`Should set 'afterViewCheckedCount' after 'ngAfterViewChecked()'`, () => {
+        // Arrange
+
+        // Act
+
+        // Assert
+        expect(component.afterViewCheckedCount).toEqual(1);    // Check with 1 instead 0
+    });
+
+    describe(`#startPomo()`, () => {
+        it(`Should call 'emitPomoState()'`, () => {
+            // Arrange
+
+            // Act
+            spyOn(component, 'emitPomoState');
+            component.startPomo();
+
+            // Assert
+            expect(component.emitPomoState).toHaveBeenCalled();
+        });
+
+        it(`Should call 'resetCounter()'`, () => {
+            // Arrange
+
+            // Act
+            spyOn(component, 'resetCounter');
+            component.startPomo();
+
+            // Assert
+            expect(component.resetCounter).toHaveBeenCalled();
+        });
+
+        it(`Should call 'pomoStateService.initPomoState()'`, () => {
+            // Arrange
+
+            // Act
+            spyOn(pomoStateService, 'initPomoState');
+            component.startPomo();
+
+            // Assert
+            expect(pomoStateService.initPomoState).toHaveBeenCalled();
+        });
+
+        it(`Should call 'startTimer()'`, () => {
+            // Arrange
+
+            // Act
+            spyOn(component, 'startTimer');
+            component.startPomo();
+
+            // Assert
+            expect(component.startTimer).toHaveBeenCalled();
+        });
+    });
+
+    describe(`#startRest()`, () => {
+        it(`Should call 'emitPomoState()'`, () => {
+            // Arrange
+
+            // Act
+            spyOn(component, 'emitPomoState');
+            component.startPomo();
+
+            // Assert
+            expect(component.emitPomoState).toHaveBeenCalled();
+        });
+
+        it(`Should call 'resetCounter()'`, () => {
+            // Arrange
+
+            // Act
+            spyOn(component, 'resetCounter');
+            component.startPomo();
+
+            // Assert
+            expect(component.resetCounter).toHaveBeenCalled();
+        });
+
+        it(`Should call 'startTimer()'`, () => {
+            // Arrange
+
+            // Act
+            spyOn(component, 'startTimer');
+            component.startPomo();
+
+            // Assert
+            expect(component.startTimer).toHaveBeenCalled();
+        });
+    });
 
 
     ////
