03eb3fb660ed093bec938607dd78b0ff2aedcfa7
Cl3r1k <Cl3r1k2@gmail.com>
2019-05-24 09:15:41 +0600

    Updates in 'Utils' class Minor changes

--- src/app/_common/utils.ts
+++ src/app/_common/utils.ts
@@ -7,19 +7,17 @@ export class Utils {
 
     public isEqualArrayOfObjects(value, valueToCheck): boolean {
 
-        let result = true;
-
         // Get the value type ('typeof' returns 'object' for arrays and objects)
         const typeValue = Object.prototype.toString.call(value);
 
         // If the two arguments are not the same type, set 'result' to false
         if (typeValue !== Object.prototype.toString.call(valueToCheck)) {
-            result = false;
+            return false;
         }
 
         // If items are not an object or array, set 'result' to false
         if (['[object Array]', '[object Object]'].indexOf(typeValue) < 0) {
-            result = false;
+            return false;
         }
 
         // Compare the length of the two items
@@ -27,22 +25,42 @@ export class Utils {
         const valueToCheckLen = typeValue === '[object Array]' ? valueToCheck.length : Object.keys(value).length;
 
         if (valueLen !== valueToCheckLen) {
-            result = false;
+            return false;
         }
 
         // Compare properties
         if (typeValue === '[object Array]') {
             for (let i = 0; i < valueLen; i++) {
-                // Compare the item
+                this.compareObjects(value[i], valueToCheck[i]);
             }
         } else {
             for (const key in value) {
                 if (value.hasOwnProperty(key)) {
-                    // Compare the item
+                    this.compareObjects(value[key], valueToCheck[key]);
                 }
             }
         }
 
+        return true;
+    }
+
+    public compareObjects(item1, item2): boolean {
+
+        // Get the object type
+        const itemType = Object.prototype.toString.call(item1);
+
+        // If an object or array, compare recursively
+        if (['[object Array]', '[object Object]'].indexOf(itemType) >= 0) {
+            if (!this.isEqualArrayOfObjects(item1, item2)) {
+                return false;
+            }
+        } else {
+            if (itemType !== Object.prototype.toString.call(item2)) {
+                return false;
+            }
+
+            //
+        }
 
         return true;
     }
