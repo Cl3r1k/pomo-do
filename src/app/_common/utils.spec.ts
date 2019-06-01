353bfe5e477d72d7661fb407aeb2815bdc2117cf
Cl3r1k <Cl3r1k2@gmail.com>
2019-06-01 09:15:37 +0600

    Updates in tests for 'Utils' class Minor changes

--- src/app/_common/utils.spec.ts
+++ src/app/_common/utils.spec.ts
@@ -3,28 +3,63 @@ import { TestBed, async } from '@angular/core/testing';
 import { Utils } from '@app/_common/utils';
 
 describe(`Class: Utils`, () => {
-    it(`Should create an instance (async)`, async(() => {
-        // Arrange
 
-        // Act
+    let _utils: Utils;
 
-        // Assert
-        expect(new Utils()).toBeTruthy();
-    }));
+    beforeEach(() => {
+        _utils = new Utils();
+    });
 
-    describe(`#randomRangeInteger():`, () => {
-        it(`Should return value from val1 to val2 (async)`, async(() => {
+    // it(`Should create an instance (async)`, async(() => {
+    //     // Arrange
+
+    //     // Act
+
+    //     // Assert
+    //     expect(_utils).toBeTruthy();
+    // }));
+
+    // describe(`#randomRangeInteger():`, () => {
+    //     it(`Should return value from val1 to val2 (async)`, async(() => {
+    //         // Arrange
+    //         let result: number;
+    //         const val1 = 0;
+    //         const val2 = 10;
+
+    //         // Act
+    //         result = _utils.randomRangeInteger(val1, val2);
+
+    //         // Assert
+    //         expect(result).toBeGreaterThanOrEqual(val1);
+    //         expect(result).toBeLessThanOrEqual(val2);
+    //     }));
+    // });
+
+    describe(`#isEqualArrayOfObjects()`, () => {
+        // it(`Shoud return 'true' for { tstKey: 'tstValue' } and { tstKey: 'tstValue' }`, () => {
+        //     // Arrange
+        //     const value = { tstKey: 'tstValue' };
+        //     const valueToCheck = { tstKey: 'tstValue' };
+
+        //     // Act
+        //     const result = _utils.isEqualArrayOfObjects(value, valueToCheck);
+        //     console.log('%c result', 'color: green;', result);
+
+        //     // Assert
+        //     expect(result).toEqual(true);
+        // });
+
+        it(`Shoud return 'false' for { tstKey: 'tstValue1' } and { tstKey: 'tstValue2' }`, () => {
             // Arrange
-            let result: number;
-            const val1 = 0;
-            const val2 = 10;
+            const value2 = { tstKey: 'tstValue1' };
+            const valueToCheck2 = { tstKey: 'tstValue2' };
 
             // Act
-            result = new Utils().randomRangeInteger(val1, val2);
+            const result2 = _utils.isEqualArrayOfObjects(value2, valueToCheck2);
+            console.log('%c result (tstValue1)', 'color: green;', result2);
 
             // Assert
-            expect(result).toBeGreaterThanOrEqual(val1);
-            expect(result).toBeLessThanOrEqual(val2);
-        }));
+            expect(result2).toEqual(true);
+        });
     });
 });
--- src/app/_common/utils.ts
+++ src/app/_common/utils.ts
@@ -63,6 +63,8 @@ export class Utils {
                 return false;
             }
 
+            console.log('%c value and valueToCheck', 'color: green;', item1, item1);
+
             // If it's a function, convert to a string and compare, otherwise, just compare
             if (itemType === '[object Function]') {
                 if (item1.toString() !== item2.toString()) {
