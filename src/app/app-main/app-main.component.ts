e67641fb5deea0d0428799e6898b8eee6a09239f
Cl3r1k <Cl3r1k2@gmail.com>
2019-07-02 09:26:12 +0600

    ✔ Fixed bug -> click on 'sync-icons' sometime (when icon not anymore in template) calls error Updates in 'AppMainComponent' (code) Minor changes

--- src/app/app-main/app-main.component.ts
+++ src/app/app-main/app-main.component.ts
@@ -371,7 +371,7 @@ export class AppMainComponent implements OnInit, OnDestroy {
 
     containerClickHandler(event) {
         // FEATURE: Here we should check, if there is some edited item -> cancel edit
-        // console.log('%ccontainerClick called with event: ', this.consoleTextColorComponent, event);
+        console.log('%ccontainerClick called with event: ', this.consoleTextColorComponent, event);
         console.log('%ccontainerClick called with event.target.innerHTML: ', this.consoleTextColorComponent, event.target.innerHTML);
         console.log('%ccontainerClick called with event.target.parentNode: ', this.consoleTextColorComponent, event.target.parentNode);
         if (event.target.className === 'content-container') {
@@ -380,7 +380,9 @@ export class AppMainComponent implements OnInit, OnDestroy {
             // console.log('%ccontainerClick called with event: ', this.consoleTextColorComponent, event);
         }
 
-        if (event.target.parentNode.className !== 'profile-menu' && event.target.parentNode.className !== 'profile-name') {
+        if (event.target.parentNode &&
+            event.target.parentNode.className !== 'profile-menu' &&
+            event.target.parentNode.className !== 'profile-name') {
             // console.log('click on profile-menu/profile-name');
             if (this.showSubmenuState) {
                 this.showSubmenuState = false;
