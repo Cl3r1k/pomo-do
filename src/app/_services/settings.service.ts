900b2c0cc16562e804f424edec4c4788d9684b82
Cl3r1k <Cl3r1k2@gmail.com>
2019-07-06 09:18:32 +0600

    ✔ Added 'SettingsData' class Updates in 'SettingsService' (code) Minor changes

--- src/app/_services/settings.service.ts
+++ src/app/_services/settings.service.ts
@@ -1,17 +1,33 @@
 import { Injectable } from '@angular/core';
 
+// Models
+import { SettingsData } from '@app/_models/settings-data';
+
 @Injectable({
     providedIn: 'root'
 })
 export class SettingsService {
 
+    keyForLocalStorage = 'settings_data';
+    settingsData = null;
+
     constructor() { }
 
     loadSettings() {
-        //
+        const data = JSON.parse(localStorage.getItem(this.keyForLocalStorage));
+
+        if (data) {
+            this.settingsData = null;
+            this.settingsData = new SettingsData();
+            // this.settingsData.start_time = data['start_time'];
+            // this.settingsData.end_time = data['end_time'];
+            // this.settingsData.rest_time = data['rest_time'];
+            // this.settingsData.status = data['status'];
+            // this.settingsData.uuid = data['uuid'];
+        }
     }
 
-    saveSettings() {
-        //
+    saveSettings(settingsData: SettingsData) {
+        localStorage.setItem(this.keyForLocalStorage, JSON.stringify(settingsData));
     }
 }
