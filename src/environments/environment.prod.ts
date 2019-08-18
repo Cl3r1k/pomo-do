7925e3199b9a7bfddfe7daa2e186d1754d7d1c64
Cl3r1k <Cl3r1k2@gmail.com>
2019-08-18 10:26:15 +0600

    Updates in 'AppMainComponent' (template) Minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,5 +11,5 @@ export const environment = {
     consoleTextColorComponent: 'color: cadetblue;',
 
     // Autoincremented build version variable
-    version: '1.5.0.1040'
+    version: '1.5.0.1041'
 };
