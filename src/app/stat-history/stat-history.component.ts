1dcd4279e7726b7b866566f8875078536018b490
Cl3r1k <Cl3r1k2@gmail.com>
2019-10-19 08:49:18 +0700

    refactor: Update in 'StatHistoryComponent' (code, template) refactor: Minor changes

--- src/app/stat-history/stat-history.component.ts
+++ src/app/stat-history/stat-history.component.ts
@@ -29,6 +29,9 @@ export class StatHistoryComponent implements OnInit {
     currentSelectedItem = 0;
     // TODO: Don't forget to change value below to real value from prefs
     dailyGoalCountPrefs = 8;
+    polylinePoints = '0,70 7.6,68, 15.2,66, 22.8,64, 30.4,62 38,60 45.6,58, 53.2,56, 60.8,54, 68.4,52 ' +
+            '76,50 83.6,48, 91.2,46, 98.8,44, 106.4,42 114,40 121.6,38, 129.2,36, 136.8,34, 144.4,32 ' +
+            '152,30 159.6,28, 167.2,26, 174.8,24, 182.4,22 190,20 197.6,18, 205.2,14, 224,07, 240,01 240,70';
 
     constructor() { }
 
