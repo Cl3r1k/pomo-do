7e3c66c026a7fc5b7297143c72459da43e7ca33c
Cl3r1k <Cl3r1k2@gmail.com>
2019-11-30 08:42:33 +0700

    refactor: Update in 'StatDetailsComponent' (template, styles) refactor: Minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,5 +11,5 @@ export const environment = {
     consoleTextColorComponent: 'color: cadetblue;',
 
     // Auto incremented build version variable
-    version: '1.5.0.1140'
+    version: '1.5.0.1141'
 };
