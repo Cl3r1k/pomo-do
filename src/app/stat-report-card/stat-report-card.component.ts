4b8e6e729ea7159c9f0d6a7209acfb91b37f9ce8
Cl3r1k <Cl3r1k2@gmail.com>
2020-03-05 08:54:13 +0700

    feat: Update in 'StatReportCardComponent' (code, template) refactor: Update in 'StatDetailsComponent' (template, styles) refactor: Minor changes

--- src/app/stat-report-card/stat-report-card.component.ts
+++ src/app/stat-report-card/stat-report-card.component.ts
@@ -6,10 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stat-report-card.component.scss']
 })
 export class StatReportCardComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
