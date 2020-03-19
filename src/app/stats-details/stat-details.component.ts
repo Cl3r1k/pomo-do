572695a2a9f733cd83a08e04d1342adda7f65b04
Cl3r1k <Cl3r1k2@gmail.com>
2020-03-19 08:48:20 +0700

    feat: Update in 'HoursChartComponent' (styles) feat: Update in 'StatDetailsComponent' (code, styles) refactor: Minor changes

--- src/app/stats-details/stat-details.component.ts
+++ src/app/stats-details/stat-details.component.ts
@@ -17,25 +17,6 @@ export class StatDetailsComponent implements OnInit {
   // *** Output emitters ***
   @Output() selectedHashtagStatDetailsComponentEmitter: EventEmitter<string> = new EventEmitter();
 
-  // hashtagsChartValues = [
-  //     {
-  //         'tagName': '#dev',
-  //         'pathD': 'M 248.4807753012208 167.36481776669302 A 100 100 0 0 0 150 50 L 150 150 Z',
-  //     },
-  //     {
-  //         'tagName': '#English',
-  //         'pathD': 'M 236.60254037844388 100 A 100 100 0 0 0 150 50 L 150 150 Z',
-  //     },
-  //     {
-  //         'tagName': '#tagName',
-  //         'pathD': 'M 184.20201433256688 56.03073792140917 A 100 100 0 0 0 150 50 L 150 150 Z',
-  //     },
-  //     {
-  //         'tagName': '#smallTag',
-  //         'pathD': 'M 184.20201433256688 56.03073792140917 A 100 100 0 0 0 150 50 L 150 150 Z',
-  //     }
-  // ];
-
   constructor() { }
 
   ngOnInit() { }
